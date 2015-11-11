8a5cb111ae75591eaa4090c6017d62aa0beacc9c
Rui Nogueira <rui.nogueira@sap.com>
2015-11-11 08:30:39 +0100

    Change sequence

--- brewinstall.sh
+++ brewinstall.sh
@@ -13,19 +13,19 @@ cd $(brew --repository)
 git reset --hard FETCH_HEAD
 
 # And now all the nice software I need
-brew-cask install mongodb
-brew-cask install macdown
-brew-cask install komodo-edit
-brew-cask install freemind
-brew-cask install vlc
 brew-cask install google-chrome
+brew-cask install komodo-edit
+brew-cask install eclipse-jee
+brew-cask install handbrake
+brew-cask install macdown
+brew-cask install vlc
+brew-cask install filezilla
+brew-cask install gimp
+brew-cask install mongodb
+brew-cask install cyberduck
+brew-cask install freemind
 brew-cask install mixxx
 brew-cask install linein
-brew-cask install handbrake
-brew-cask install filezilla
-brew-cask install cyberduck
-brew-cask install gimp
-brew-cask install eclipse-jee
 
 sudo -s
 brew-cask install xampp
