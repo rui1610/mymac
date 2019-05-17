4abc3d18ac3640253ed695a243f839a1bbd102ea
rui1610 <rui1610@users.noreply.github.com>
2019-05-17 09:59:45 +0200

    Update brewinstall.sh

--- brewinstall.sh
+++ brewinstall.sh
@@ -17,13 +17,11 @@ git reset --hard FETCH_HEAD
 
 # And now all the nice software I need
 brew install node@6
-brew install Caskroom/cask/freemind
-brew install Caskroom/cask/google-chrome
-brew install Caskroom/cask/komodo-edit
-brew install Caskroom/cask/handbrake
-#brew install Caskroom/cask/handbrakecli
-brew install Caskroom/cask/vagrant
-brew install Caskroom/cask/virtualbox
+brew cask install google-chrome
+brew cask install komodo-edit
+#brew cask install handbrakecli
+brew cask install vagrant
+
 brew cask install macdown
 brew tap cloudfoundry/tap
 brew install cf-cli
@@ -39,23 +37,29 @@ brew cask install caskroom/cask/openemu
 brew install ffmpeg --with-fdk-aac --with-sdl2 --with-freetype --with-libass --with-libvorbis --with-libvpx --with-opus --with-x265
 # usage: ffmpeg -i input.mkv -codec copy output.mp4
 
-#brew-cask install gimp
+brew cask install gimp
+brew cask install handbrake
+
 #brew-cask install mongodb
 #brew-cask install cyberduck
 
-#brew-cask install mixxx
-#brew-cask install Caskroom/cask/metaz
-#brew-cask install Caskroom/cask/musicbrainz-picard
-#brew-cask install Caskroom/cask/mixxx
-#brew-cask install linein
-#brew-cask install Caskroom/cask/xquartz
-#brew-cask install homebrew/science/octave
+brew cask install mixxx
+brew cask install metaz
+brew cask install musicbrainz-picard
+brew cask install linein
+brew cask install xquartz
+brew cask install octave
 
 brew cask install caskroom/cask/flightgear
 
 # Install also dupeGuru, DateTree, FlightGear, ShowHiddenFiles
 
 #sudo -s
-brew-cask install virtualbox
+brew cask install virtualbox
+# Cool screen saver
+brew cask install aerial
+
+brew cask install freemind
+
 
 gem install jekyll
